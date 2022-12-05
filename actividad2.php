c3398ffe1f101a5e917bbd7c150b308a9a25793f
DavidC382 <david382777@gmail.com>
2022-12-05 15:57:00 -0500

    Interfaz II

---
+++ actividad2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body {
+        
+            background-image: url("https://www.lavanguardia.com/files/og_thumbnail/uploads/2021/04/09/606ffcccb78a0.jpeg");
+            background-size: cover;
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h2>Formulario de registro</h2>
+    </center>
+    <form>
+            <div class="container mt-5" style="background-color: rgba(148, 216, 236, 0.247)">
+            
+                <div class="input-group mb-3 mt-5">
+                    <span class="input-group-text" id="basic-addon1">Primer Nombre</span>
+                    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">  
+                    <span class="input-group-text" id="basic-addon1">Segundo Nombre</span>
+                    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+                </div>
+                
+                <div class="input-group mb-3">  
+                    <span class="input-group-text" id="basic-addon1">Primer Apellido</span>
+                    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+                </div>
+                
+                <div class="input-group mb-3">  
+                    <span class="input-group-text" id="basic-addon1">Segundo Apellido</span>
+                    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">  
+                    <span class="input-group-text" id="basic-addon1">Edad</span>
+                    <input type="text" class="form-control" aria-describedby="basic-addon1">
+                </div>
+                <div class="input-group mb-3">  
+                    <span class="input-group-text" id="basic-addon1">Ciudad</span>
+                    <input type="text" class="form-control"  aria-describedby="basic-addon1">
+                </div>
+            
+                    <button type="button" class="btn btn-success">Agregar</button>
+                    <button type="button" class="btn btn-success">Modificar</button>
+                    <button type="button" class="btn btn-success">Eliminar</button>
+            </div>
+        </form>
+
+
+    </div>
+    
+    
+</body>
+</html>
